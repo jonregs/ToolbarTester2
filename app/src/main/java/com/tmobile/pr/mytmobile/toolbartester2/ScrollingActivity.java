cf154b8e2bfc46ff9e69824a4593e47661a90562
Regalado, Jonathan <Jonathan.Regalado7@T-Mobile.com>
2017-10-05 15:58:12 -0700

    Scroll view fixes and messaging icon in toolbar

--- app/src/main/java/com/tmobile/pr/mytmobile/toolbartester2/ScrollingActivity.java
+++ app/src/main/java/com/tmobile/pr/mytmobile/toolbartester2/ScrollingActivity.java
@@ -15,7 +15,9 @@ public class ScrollingActivity extends AppCompatActivity {
     private MyAdapter myAdapter;
     private RecyclerViewScrollListener scrollListener;
     private Toolbar toolbar;
-    private int viewHeight;
+    private float computedValue;
+    private float cardHeight;
+    private float scrollY;
 
     String TAG = "ToolbarTester2";
 
@@ -50,6 +52,22 @@ public class ScrollingActivity extends AppCompatActivity {
         dataModel.add(new Model("Hello World", "inge all your favorite shows at no extra charge when you sign up for 2+ lines of T-Mobile ONE.\n" +
                 "        Offer subject to change. Receive Netflix Standard 2-screen (up to $9.99/mo. value) while you maintain\n" +
                 "        2+ qual’g T-Mobile ONE lines in good standing. Netflix account  compatible device required.g. Netflix account  compatible device required.g. Netflix account  compatible device required.g. Netflix account  compatible device required."));
+        dataModel.add(new Model("Hello World", "inge all your favorite shows at no extra charge when you sign up for 2+ lines of T-Mobile ONE.\n" +
+                "        Offer subject to change. Receive Netflix Standard 2-screen (up to $9.99/mo. value) while you maintain\n" +
+                "        2+ qual’g T-Mobile ONE lines in good standing. Netflix account  compatible device required.g. Netflix account  compatible device required.g. Netflix account  compatible device required.g. Netflix account  compatible device required."));
+        dataModel.add(new Model("Hello World", "inge all your favorite shows at no extra charge when you sign up for 2+ lines of T-Mobile ONE.\n" +
+                "        Offer subject to change. Receive Netflix Standard 2-screen (up to $9.99/mo. value) while you maintain\n" +
+                "        2+ qual’g T-Mobile ONE lines in good standing. Netflix account  compatible device required.g. Netflix account  compatible device required.g. Netflix account  compatible device required.g. Netflix account  compatible device required."));
+        dataModel.add(new Model("Hello World", "inge all your favorite shows at no extra charge when you sign up for 2+ lines of T-Mobile ONE.\n" +
+                "        Offer subject to change. Receive Netflix Standard 2-screen (up to $9.99/mo. value) while you maintain\n" +
+                "        2+ qual’g T-Mobile ONE lines in good standing. Netflix account  compatible device required.g. Netflix account  compatible device required.g. Netflix account  compatible device required.g. Netflix account  compatible device required."));
+        dataModel.add(new Model("Hello World", "inge all your favorite shows at no extra charge when you sign up for 2+ lines of T-Mobile ONE.\n" +
+                "        Offer subject to change. Receive Netflix Standard 2-screen (up to $9.99/mo. value) while you maintain\n" +
+                "        2+ qual’g T-Mobile ONE lines in good standing. Netflix account  compatible device required.g. Netflix account  compatible device required.g. Netflix account  compatible device required.g. Netflix account  compatible device required."));
+        dataModel.add(new Model("Hello World", "inge all your favorite shows at no extra charge when you sign up for 2+ lines of T-Mobile ONE.\n" +
+                "        Offer subject to change. Receive Netflix Standard 2-screen (up to $9.99/mo. value) while you maintain\n" +
+                "        2+ qual’g T-Mobile ONE lines in good standing. Netflix account  compatible device required.g. Netflix account  compatible device required.g. Netflix account  compatible device required.g. Netflix account  compatible device required."));
+
 
 
         myAdapter = new MyAdapter(dataModel);
@@ -59,6 +77,7 @@ public class ScrollingActivity extends AppCompatActivity {
     private void setUpToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         toolbar.getBackground().setAlpha(0);
     }
@@ -68,32 +87,62 @@ public class ScrollingActivity extends AppCompatActivity {
         public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
             super.onScrollStateChanged(recyclerView, newState);
 
-            int scrollY = recyclerView.computeVerticalScrollOffset();
-            int cardHeight = getCardHeight();
-
-            int computedValue = ((scrollY/cardHeight)*(90-20))+20;
-
-            Log.i(TAG, "onScrollStateChanged: " + computedValue);
-            toolbar.getBackground().setAlpha(computedValue);
         }
 
         @Override
         public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
             super.onScrolled(recyclerView, dx, dy);
 
+            scrollY = recyclerView.computeVerticalScrollOffset();
+            cardHeight = 419;
+            computedValue = ((Math.abs(scrollY)/Math.abs(cardHeight))*(229-51))+51;
+
+            Log.i(TAG, "cardHeight: " + cardHeight);
+            Log.i(TAG, "scrollY: " + scrollY);
+            Log.i(TAG, "computedValue: " + (int) computedValue);
+
+            if (scrollY <= cardHeight) {
+                toolbar.getBackground().mutate().setAlpha((int) computedValue);
+            } else if (cardHeight > scrollY)
+            {
+                computedValue = 229;
+                toolbar.getBackground().mutate().setAlpha((int) computedValue);
+            }
+
         }
     }
 
     private int getCardHeight() {
         int viewHeight = myAdapter.getViewHeight();
-        Log.i(TAG, "getCardHeight: " + viewHeight);
         return viewHeight;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        if (recyclerView == null) {
+            return;
+        }
+        recyclerView.addOnScrollListener(scrollListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "onResume: computedValue" + computedValue );
+        Log.i(TAG, "onResume: scrollY" + scrollY );
+        Log.i(TAG, "onResume: cardHeight" + cardHeight );
 
         recyclerView.addOnScrollListener(scrollListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (recyclerView == null) {
+            return;
+        }
+        recyclerView.removeOnScrollListener(scrollListener);
     }
 }
